1541ca6411a5452e0ffe1b1b0cacde8e0be222fa
Michael Fedell <michaelfedell14@gmail.com>
2019-12-04 15:11:13 -0600

    Bugfix rules script and doc lemma to rule lemma
    Previously, words in doc were being lemmatized, but the actual
    keywords in rule mapping were not. This resulted in poor matching
    and not representative of actual lemmatization
    
    Still not possible to use both lemmatization AND expansion with
    current implementation (would need to init separate
    expanded_lemma_cat_map)

--- src/rules.py
+++ src/rules.py
@@ -1,21 +1,21 @@
-import copy
+import multiprocessing as mp
 import os
 import sys
+import typing as t
 from collections import Counter, defaultdict
 from pathlib import Path
-import multiprocessing as mp
-import typing as t
+import re
 
 import spacy
-from tqdm import tqdm
 from gensim.models import KeyedVectors
+from tqdm import tqdm
 
 nlp = spacy.load("en_core_web_sm")
 
 # Simple rules mapping category to list of keywords
 RULES = {
-    'product': ['beautiful', '', ''],
-    'build_quality': ['broken', 'bent', 'rip'],
+    'product': ['beautiful', 'perfect'],
+    'build_quality': ['broken', 'bent', 'quality'],
     'pricing': ['cost', 'cheap', 'expensive'],
     'order_fulfillment': ['deliver', 'pack', 'ship'],
     'customer_service': ['service', 'email', 'refund'],
@@ -33,50 +33,6 @@ PRIORITY = [
     'holiday_seasonal',
     'product'
 ]
-
-# Invert rules so that keywords map to a category
-default_cat_map = {kw: c for c, r in RULES.items() for kw in r}
-
-
-def classify(doc: str, use_lemma: bool = False, expand_cat_map: bool = False) -> str:
-    """
-    Classify a document by matching its words to a mapping of keywords to category
-
-    :param doc: text to be categorized according to cat_map
-    :param use_lemma: will use a lemmatizer to simplify words in doc if true
-    :param expand_cat_map: will expand the default_cat_map to include embedded neighbors if true
-    :return: string corresponding to category of document (one of keys in RULES)
-    """
-    if expand_cat_map:
-        all_kws = [w for cat in RULES for w in RULES[cat]]  # flatten all keywords
-        cat_map = expand_rules(w2v_neighbors(all_kws))  # use kw neighbors to expand category mapping
-    else:
-        cat_map = default_cat_map
-    if use_lemma:
-        doc = nlp(doc)
-        doc = [tok.lemma_ for tok in doc]
-    else:
-        doc = doc.lower().split()
-    keys = [cat_map.get(w, '') for w in doc]
-    keys = list(filter(None, keys))
-    count = Counter(keys)
-    top = count.most_common(1)
-    return top[0][0] if top else 'unk'  # return key with highest count
-
-
-def classify_naive(doc: str) -> str:
-    """Classify a document by direct matching of keywords for each category"""
-    return classify(doc, use_lemma=False, expand_cat_map=False)
-
-
-def classify_lemma(doc: str) -> str:
-    """Classify a document by keyword frequency but examining word lemmas for more general match"""
-    return classify(doc, use_lemma=True, expand_cat_map=False)
-
-
-def classify_embedded(doc: str) -> str:
-    """Classify a document by keyword frequency using neighboring keywords mined from embedding space"""
-    return classify(doc, use_lemma=True, expand_cat_map=True)
 
 
 def w2v_neighbors(words: t.List[str],
@@ -115,13 +71,69 @@ def expand_rules(neighbors: t.Dict[str, t.List[str]]):
 
             # Map the keyword and all of its neighbors to the current category
             for w in neighbors[kw]:
-                if cat_map.get(w):
-                    print(f'WARNING: expanded kw: {w} already exists in {cat_map.get(w)} category...'
-                          f'To avoid ambiguity, this word will be removed from mapping entirely')
+                if cat_map.get(w) != cat:
+                    print(f'WARNING: expanded kw: {w} found in {cat} but '
+                          f'already exists in {cat_map.get(w)}... To avoid ambiguity, '
+                          f'this word will be removed from mapping entirely')
                     dupes.append(w)  # remember that this word is ambiguous - remove once done
                 cat_map[w] = cat
+    for dupe in set(dupes):
+        del cat_map[dupe]
 
     return cat_map
+
+
+# Invert rules so that keywords map to a category
+default_cat_map = {kw: c for c, r in RULES.items() for kw in r}
+# Re-key cat_map with keyword lemmas
+lemma_cat_map = {nlp(kw)[0].lemma_: cat for kw, cat in default_cat_map.items()}
+# flatten all keywords and add keyword neighbors to cat_map
+all_kws = [w for cat in RULES for w in RULES[cat]]
+expanded_cat_map = expand_rules(w2v_neighbors(all_kws))
+
+
+def classify(doc: str, use_lemma: bool = False, expand_cat_map: bool = False) -> str:
+    """
+    Classify a document by matching its words to a mapping of keywords to category
+
+    :param doc: text to be categorized according to cat_map
+    :param use_lemma: will use a lemmatizer to simplify words in doc if true
+    :param expand_cat_map: expand the default_cat_map to include embedded neighbors if true
+    :return: string corresponding to category of document (one of keys in RULES)
+    """
+    # use kw neighbors to expand category mapping
+    cat_map = expanded_cat_map if expand_cat_map else default_cat_map
+    # split doc into tokens
+    if use_lemma:
+        # Use spacy lemmatizer to simplify words in both doc and map
+        tokens = [tok.lemma_ for tok in nlp(doc)]
+        cat_map = lemma_cat_map
+    else:
+        # lowercase, remove punctuation, and split words
+        non_alpha = re.compile(r'[^a-z]')
+        tokens = [non_alpha.sub('', tok.strip()) for tok in doc.lower().split()]
+
+    keys = [cat_map.get(w, '') for w in tokens]
+    keys = list(filter(None, keys))
+
+    count = Counter(keys)
+    top = count.most_common(1)
+    return top[0][0] if top else 'unk'  # return key with highest count
+
+
+def classify_naive(doc: str) -> str:
+    """Classify a document by direct matching of keywords for each category"""
+    return classify(doc, use_lemma=False, expand_cat_map=False)
+
+
+def classify_lemma(doc: str) -> str:
+    """Classify a document by keyword frequency, examining word lemmas for more general match"""
+    return classify(doc, use_lemma=True, expand_cat_map=False)
+
+
+def classify_embedded(doc: str) -> str:
+    """Classify a document by keyword frequency including neighbors mined from embedding space"""
+    return classify(doc, use_lemma=True, expand_cat_map=True)
 
 
 if __name__ == '__main__':
@@ -130,7 +142,8 @@ if __name__ == '__main__':
     assert review_path.exists()
     with review_path.open() as f:
         header = f.readline()
-        reviews = [l.split(',')[1] for l in f.readlines()]
+        # First field is document_id; text may contain extra commas or quotation marks
+        reviews = [','.join(l.split(',')[1:]).replace('"', '') for l in f.readlines()]
 
     methods = {
         'naive': classify_naive,
@@ -142,5 +155,5 @@ if __name__ == '__main__':
         print('Producing labels via', method, 'strategy')
         with mp.Pool(mp.cpu_count() - 1) as p:
             labels = list(tqdm(p.imap(methods[method], reviews), total=len(reviews)))
-        with open(f'../data/{method}/labeled_reviews.csv') as f:
-            f.writelines(','.join(l) + '\n' for l in zip(labels, reviews))
+        with open(f'../data/{method}/labeled_{review_path.name}', 'w', encoding='utf-8') as f:
+            f.writelines(','.join(l) for l in zip(labels, reviews))
