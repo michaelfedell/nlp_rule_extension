981ed223be21319e750450223725bc0297307ee0
Michael Fedell <michaelfedell14@gmail.com>
2019-12-04 04:16:08 -0600

    Add w2v neighbor to rules for expanded classification

--- src/rules.py
+++ src/rules.py
@@ -1,10 +1,14 @@
+import copy
+import os
 import sys
-from collections import Counter
+from collections import Counter, defaultdict
 from pathlib import Path
 import multiprocessing as mp
+import typing as t
 
 import spacy
 from tqdm import tqdm
+from gensim.models import KeyedVectors
 
 nlp = spacy.load("en_core_web_sm")
 
@@ -13,10 +17,10 @@ RULES = {
     'product': ['beautiful', '', ''],
     'build_quality': ['broken', 'bent', 'rip'],
     'pricing': ['cost', 'cheap', 'expensive'],
-    'order_fulfillment': ['deliver', 'late', 'ship'],
+    'order_fulfillment': ['deliver', 'pack', 'ship'],
     'customer_service': ['service', 'email', 'refund'],
     'registry': ['wedding', 'shower', 'register'],
-    'holiday_seasonal': ['season', 'christmas', 'summer']
+    'holiday_seasonal': ['season', 'holiday', 'summer']
 }
 
 # Can specify priority in case of ties?
@@ -31,12 +35,28 @@ PRIORITY = [
 ]
 
 # Invert rules so that keywords map to a category
-cat_map = {l: k for k, v in RULES.items() for l in v}
+default_cat_map = {kw: c for c, r in RULES.items() for kw in r}
 
 
-def classify_naive(doc: str) -> str:
-    """Classify a document by direct matching of keywords for each category"""
-    doc = doc.lower().split()
+def classify(doc: str, use_lemma: bool = False, expand_cat_map: bool = False) -> str:
+    """
+    Classify a document by matching its words to a mapping of keywords to category
+
+    :param doc: text to be categorized according to cat_map
+    :param use_lemma: will use a lemmatizer to simplify words in doc if true
+    :param expand_cat_map: will expand the default_cat_map to include embedded neighbors if true
+    :return: string corresponding to category of document (one of keys in RULES)
+    """
+    if expand_cat_map:
+        all_kws = [w for cat in RULES for w in RULES[cat]]  # flatten all keywords
+        cat_map = expand_rules(w2v_neighbors(all_kws))  # use kw neighbors to expand category mapping
+    else:
+        cat_map = default_cat_map
+    if use_lemma:
+        doc = nlp(doc)
+        doc = [tok.lemma_ for tok in doc]
+    else:
+        doc = doc.lower().split()
     keys = [cat_map.get(w, '') for w in doc]
     keys = list(filter(None, keys))
     count = Counter(keys)
@@ -44,21 +64,64 @@ def classify_naive(doc: str) -> str:
     return top[0][0] if top else 'unk'  # return key with highest count
 
 
+def classify_naive(doc: str) -> str:
+    """Classify a document by direct matching of keywords for each category"""
+    return classify(doc, use_lemma=False, expand_cat_map=False)
+
+
 def classify_lemma(doc: str) -> str:
     """Classify a document by keyword frequency but examining word lemmas for more general match"""
-    doc = nlp(doc)
-    lemma = [t.lemma_ for t in doc]
-    keys = [cat_map.get(w, '') for w in lemma]
-    keys = list(filter(None, keys))
-    count = Counter(keys)
-    top = count.most_common(1)
-    return top[0][0] if top else 'unk'  # return key with highest count
+    return classify(doc, use_lemma=True, expand_cat_map=False)
 
 
 def classify_embedded(doc: str) -> str:
-    """Classify a document with the expanded embedding vectors for category keywords"""
-    # TODO: implement
-    return 'unk'
+    """Classify a document by keyword frequency using neighboring keywords mined from embedding space"""
+    return classify(doc, use_lemma=True, expand_cat_map=True)
+
+
+def w2v_neighbors(words: t.List[str],
+                  wv_path: str = '../artifacts/w2v_embeddings.wv',
+                  k: int = 7) -> t.Dict[str, t.List[str]]:
+    """Find the top k neighbors for each word in a list using a saved Word2Vec model"""
+    assert os.path.exists(wv_path)
+    word_vec = KeyedVectors.load(wv_path)
+
+    word_map = defaultdict(list)
+    for word in words:
+        try:
+            neighbors = [tup[0] for tup in word_vec.most_similar(positive=[word], topn=k)]
+        except KeyError:
+            print(f'Keyword "{word}" was not found in WV vocab and thus will not be expanded')
+            neighbors = []
+
+        word_map[word] = neighbors
+
+    return word_map
+
+
+def expand_rules(neighbors: t.Dict[str, t.List[str]]):
+    """
+    Expand the set of keywords for each category based on word neighbors
+
+    :param neighbors: maps each keyword in RULES to list of neighbors
+    :return: new cat_map with expanded entries
+    """
+
+    cat_map = {}
+    dupes = []
+    for cat in RULES:
+        for kw in RULES[cat]:  # kw is each keyword for each category in original ruleset
+            cat_map[kw] = cat  # ensure original word is present in expanded mapping
+
+            # Map the keyword and all of its neighbors to the current category
+            for w in neighbors[kw]:
+                if cat_map.get(w):
+                    print(f'WARNING: expanded kw: {w} already exists in {cat_map.get(w)} category...'
+                          f'To avoid ambiguity, this word will be removed from mapping entirely')
+                    dupes.append(w)  # remember that this word is ambiguous - remove once done
+                cat_map[w] = cat
+
+    return cat_map
 
 
 if __name__ == '__main__':
@@ -79,5 +142,5 @@ if __name__ == '__main__':
         print('Producing labels via', method, 'strategy')
         with mp.Pool(mp.cpu_count() - 1) as p:
             labels = list(tqdm(p.imap(methods[method], reviews), total=len(reviews)))
-        with open(f'data/{method}/labeled_reviews.csv') as f:
+        with open(f'../data/{method}/labeled_reviews.csv') as f:
             f.writelines(','.join(l) + '\n' for l in zip(labels, reviews))
